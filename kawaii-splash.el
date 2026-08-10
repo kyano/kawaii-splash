d55aed3c4148acc1d93b0ae79a219e634e1e75a0
Anho Ki <anho.ki@linecorp.com>
2026-08-10 18:32:51 +0900

    Replace `all-the-icons' with `nerd-icons'

--- kawaii-splash.el
+++ kawaii-splash.el
@@ -5,8 +5,8 @@
 ;; Author: Anho Ki
 ;; Maintainer: Anho Ki
 ;; URL: https://github.com/kyano/kawaii-splash
-;; Version: 0.0.1
-;; Package-Requires: ((emacs "29.1") (all-the-icons "6.0.0") (vterm) (gptel))
+;; Version: 0.0.2
+;; Package-Requires: ((emacs "29.1") (nerd-icons))
 
 ;; This file is not part of GNU Emacs.
 
@@ -29,104 +29,176 @@
 
 ;;; Code:
 
-(require 'all-the-icons)
+(require 'nerd-icons)
 
 (defconst kawaii-splash/menu-items
-  `(("Open a File"
+  `(("Emacs Tutorial"
+     "Learn basic keystroke commands"
+     ,(lambda (_button) (help-with-tutorial)))
+    ("View Emacs Manual"
+     "View the Emacs manual using Info"
+     ,(lambda (_button) (info-emacs-manual)))
+    ("Open a File"
      "Specify a new file's name, to edit the file"
-     ,(lambda (_button) (call-interactively 'find-file))
-     ,(all-the-icons-fontawesome-4 "file-text-o"))
-    ("Open a Directory"
-     "Open a directory, to operate on its files"
-     ,(lambda (_button) (call-interactively 'dired))
-     ,(all-the-icons-fontawesome-4 "folder-open-o"))
-    ("Open Treemacs"
-     "Start treemacs mode and show the sidebar"
-     ,(lambda (_button) (treemacs))
-     ,(all-the-icons-icon-for-mode 'treemacs-mode))
-    ("Open gptel menu"
-     "Start gptel and show the transient menu"
-     ,(lambda (_button) (call-interactively #'gptel-menu))
-     ,(all-the-icons-fluentui-system-icons "brain_circuit" :style 'filled))
+     ,(lambda (_button) (call-interactively #'find-file))
+     ,(nerd-icons-faicon "nf-fa-file_o" :height 1.1))
+    ("Open Home Directory"
+     "Open your home directory, to operate on its files"
+     ,(lambda (_button) (dired "~"))
+     ,(nerd-icons-faicon "nf-fa-folder_o" :height 1.1))
+    ,(when (fboundp 'gptel)
+       (list "Open gptel menu"
+             "Start gptel and show the transient menu"
+             (lambda (_button) (call-interactively #'gptel-menu))
+             (nerd-icons-mdicon "nf-md-chat_alert" :height 1.1)))
     ("Open Terminal"
-     "Start an interactive terminal buffer"
+     "Start a terminal-emulator in a new buffer"
      ,(lambda (_button)
         (cond ((fboundp 'ghostel) (ghostel t))
               ((fboundp 'vterm) (vterm t))
               ((fboundp 'eat) (eat t))
-              (t (call-interactively 'term))))
-     ,(all-the-icons-icon-for-mode 'term-mode))
-    ("Package management"
-     "Display a list of packages"
-     ,(lambda (_button) (list-packages))
-     ,(all-the-icons-icon-for-mode 'package-menu-mode))
+              (t (call-interactively #'term))))
+     ,(nerd-icons-devicon "nf-dev-terminal" :height 1.1))
     ("Customize"
-     "Change settings"
+     "Select a customization buffer which you can use to set user options"
      ,(lambda (_button) (customize))
-     ,(all-the-icons-fontawesome-4 "gears"))
+     ,(nerd-icons-faicon "nf-fa-gears" :height 1.1))
     (,(concat "Edit `" user-emacs-directory "init.el" "'")
-     "Open the default Init file"
+     "Open `init.el'"
      ,(lambda (_button) (find-file (concat user-emacs-directory "init.el")))
-     ,(all-the-icons-fontawesome-4 "gears"))
+     ,(nerd-icons-faicon "nf-fa-gears" :height 1.1))
+    ("Explore Packages"
+     "Explore, install and remove Emacs packages (requires Internet connection)"
+     ,(lambda (_button) (call-interactively #'list-packages))
+     ,(nerd-icons-faicon "nf-fa-archive" :height 1.1))
     ("Toggle frame maximized"
-     "Toggle the frame to maximized or vice-versa"
+     "Toggle maximization state"
      ,(lambda (_button) (toggle-frame-maximized))
-     ,(all-the-icons-fontawesome-4 "window-maximize"))
+     ,(nerd-icons-faicon "nf-fa-window_maximize" :height 1.1))
     ("Toggle frame fullscreen"
-     "Toggle the frame to fullscreen or vice-versa"
+     "Toggle fullscreen state"
      ,(lambda (_button) (toggle-frame-fullscreen))
-     ,(all-the-icons-fluentui-system-icons "full_screen_maximize" :style 'filled))))
-
-(defun kawaii-splash/scale-svg-icon (icon scale)
-  "Scale the embedded SVG icon of ICON to SCALE.
-
-ICON must be a returned character from `all-the-icons' that has a
-property `display'."
-
-  (let ((img (get-text-property 0 'display icon)))
-    (setf (image-property img :scale) scale)
-    (set-text-properties 0 0 '('display img) icon)
-    icon))
+     ,(nerd-icons-octicon "nf-oct-screen_full" :height 1.1))))
 
 (defun kawaii-splash/splash-screen ()
-  "Generate a buffer for splash screen.
-
-If `fancy-splash-image' is not the default value(nil), the bundled
-image is used instead."
+  "Generate a buffer for splash screen."
 
   (let ((splash-buffer (get-buffer-create "*splash*")))
     (with-current-buffer splash-buffer
       (let ((inhibit-read-only t))
         (erase-buffer)
+        (setq default-directory command-line-default-directory)
+        (make-local-variable 'startup-screen-inhibit-startup-screen)
+
+        ;; Customized `fancy-splash-head'
         (let* ((image-file (fancy-splash-image-file))
-               (img (create-image image-file)))
-          (when (and (memq 'xpm img)
-                     (eq (frame-parameter nil 'background-mode) 'dark))
-            (setq img (append img '(:color-symbols (("#000000" . "gray30"))))))
-          (insert-image img)
-          (insert "\n\n")
-          (add-hook 'window-configuration-change-hook
-                    #'(lambda ()
-                        (when (equal (current-buffer)
-                                     splash-buffer)
-                          (setf (image-property img :max-width)
-                                (window-pixel-width))))))
-        (fancy-splash-insert :face '(variable-pitch (:height 1.2) font-lock-comment-face)
-                             "Welcome to GNU Emacs, one component of the GNU operating system.\n")
-        (insert "\n\n")
+               (img (create-image image-file))
+               (image-width (and img (car (image-size img))))
+               (window-width (window-width)))
+          (when img
+            (when (> window-width image-width)
+              (let ((text-width 80)
+                    (adjust-left 3))
+                (insert (propertize " " 'display
+                                    `(spaced :align-to (+ ,(- (/ text-width 2)
+                                                              adjust-left)
+                                                          (-0.5 . ,img)))))))
+            (when (and (memq 'xpm img)
+                       (eq (frame-parameter nil 'background-mode) 'dark))
+              (setq img (append img '(:color-symbols (("#000000" . "gray30"))))))
+            (insert-image img)
+            (insert "\n\n")
+            (add-hook 'window-configuration-change-hook
+                      #'(lambda ()
+                          (when (equal (current-buffer)
+                                       splash-buffer)
+                            (setf (image-property img :max-width)
+                                  (window-pixel-width)))))))
+        ;; End of customized `fancy-splash-head'
+
+        ;; Customized `fancy-startup-text'
+        (fancy-splash-insert
+         :face '(variable-pitch (:height 1.2) font-lock-comment-face)
+         "Welcome to "
+         :link
+         `("GNU Emacs"
+           ,(lambda (_button)
+              (let ((browse-url-browser-function 'eww-browse-url))
+                (browse-url "https://www.gnu.org/software/emacs/")))
+           "Browse https://www.gnu.org/software/emacs/")
+         ", one component of the "
+         :link
+         (lambda ()
+           (if (eq system-type 'gnu/linux)
+               `("GNU/Linux"
+                 ,(lambda (_button)
+                    (let ((browse-url-browser-function 'eww-browse-url))
+                      (browse-url "https://www.gnu.org/gnu/linux-and-gnu.html")))
+                 "Browse https://www.gnu.org/gnu/linux-and-gnu.html")
+             `("GNU"
+               ,(lambda (_button)
+                  (let ((browse-url-browser-function 'eww-browse-url))
+                    (browse-url "https://www.gnu.org/gnu/thegnuproject.html")))
+               "Browse https://www.gnu.org/gnu/thegnuproject.html")))
+         " operating system.\n")
+        (fancy-splash-insert :face 'variable-pitch "\n")
+        ;; End of customized `fancy-startup-text'
+
         (dolist (menu-item kawaii-splash/menu-items)
-          (let ((name (nth 0 menu-item))
-                (description (nth 1 menu-item))
-                (function (nth 2 menu-item))
-                (bullet-icon (nth 3 menu-item)))
-            (fancy-splash-insert :face '(variable-pitch (:height 1.1))
-                                 "\t"
-                                 (if (not bullet-icon)
-                                     (format "%s\t" (kawaii-splash/scale-svg-icon (all-the-icons-file-icons "emacs") 1.1))
-                                   (format "%s\t" (kawaii-splash/scale-svg-icon bullet-icon 1.1)))
-                                 :link `(,name ,function ,description)
-                                 "\n")))
-        (insert "\n\n")
+          (when menu-item
+            (let ((name (nth 0 menu-item))
+                  (description (nth 1 menu-item))
+                  (function (nth 2 menu-item))
+                  (bullet-icon (nth 3 menu-item)))
+              (fancy-splash-insert :face '(variable-pitch (:height 1.1))
+                                   "\t")
+              (insert (if bullet-icon
+                          bullet-icon
+                        (nerd-icons-sucicon "nf-custom-emacs" :height 1.1)))
+              (fancy-splash-insert :face '(variable-pitch (:height 1.1))
+                                   "\t"
+                                   :link
+                                   `(,name ,function ,description)
+                                   "\n"))))
+        (skip-chars-backward "\n")
+        (delete-region (point) (point-max))
+        (fancy-splash-insert :face 'variable-pitch "\n\n")
+
+        ;; Customized `fancy-start-up-tail'
+        (fancy-splash-insert
+         :face 'variable-pitch "To quit a partially entered command, type "
+         :face 'default "Control-g"
+         :face 'variable-pitch ".\n")
+        (fancy-splash-insert :face '(variable-pitch bold) "New to Emacs?")
+        (fancy-splash-insert
+         :face 'variable-pitch
+         "  Consider enabling "
+         :link `("newcomer presets"
+                 ,(lambda (_button) (info "(emacs) Newcomers Theme")))
+         " by clicking this checkbox:  ")
+        (let ((checked (create-image "checked.xpm"
+                                     nil nil :ascent 'center))
+              (unchecked (create-image "unchecked.xpm"
+                                       nil nil :ascent 'center))
+              (enabled (custom-theme-enabled-p 'newcomers-presets)))
+          (insert-button
+           " "
+           :on-glyph checked
+           :off-glyph unchecked
+           'checked enabled
+           'display (if enabled checked unchecked)
+           'follow-link t
+           'action (lambda (button)
+                     (if (overlay-get button 'checked)
+                         (progn (overlay-put button 'checked nil)
+                                (overlay-put button 'display
+                                             (overlay-get button :off-glyph))
+                                (disable-theme 'newcomers-presets))
+                       (overlay-put button 'checked t)
+                       (overlay-put button 'display
+                                    (overlay-get button :on-glyph))
+                       (load-theme 'newcomers-presets)))))
+        (fancy-splash-insert :face 'variable-pitch "\n")
         (when auto-save-list-file-prefix
           (let ((dir  (file-name-directory auto-save-list-file-prefix))
                 (name (file-name-nondirectory auto-save-list-file-prefix))
@@ -135,53 +207,72 @@ image is used instead."
                  (setq files (directory-files dir nil (concat "\\`" name) t))
                  (fancy-splash-insert :face '(variable-pitch font-lock-comment-face)
                                       (if (= (length files) 1)
-                                          "An auto-save file list was found.  "
-                                        "Auto-save file lists were found.  ")
+                                          "\nAn auto-save file list was found.  "
+                                        "\nAuto-save file lists were found.  ")
                                       "If an Emacs session crashed recently,\ntype "
                                       :link `("M-x recover-session RET"
-                                              ,(lambda (_button) (call-interactively 'recover-session)))
-                                      " to recover the files you were editing.\n"
-                                      "\n\n"))))
-        (fancy-splash-insert :face '(variable-pitch font-lock-builtin-face)
-                             (emacs-version) "\n")
+                                              ,(lambda (_button)
+                                                 (call-interactively
+                                                  'recover-session)))
+                                      " to recover the files you were editing.\n"))))
+        (fancy-splash-insert :face 'variable-pitch "\n")
+        (save-restriction
+          (narrow-to-region (point) (point))
+          (fancy-splash-insert :face '(variable-pitch font-lock-builtin-face)
+                               "This is "
+                               (emacs-version)
+                               "\n")
+          (fill-region (point-min) (point-max)))
         (let ((wsl-version (condition-case nil
-                               (process-lines "/usr/bin/wslinfo" "--wsl-version")
+                               (process-lines "/usr/bin/wslinfo"
+                                              "--wsl-version")
                              (error nil)))
               (os-icon))
           (cond ((eq system-type 'gnu/linux)
-                 (setq os-icon (all-the-icons-fontawesome-4 "linux")))
+                 (setq os-icon (nerd-icons-faicon "nf-fa-linux" :height 0.8)))
                 ((eq system-type 'darwin)
-                 (setq os-icon (all-the-icons-fontawesome-4 "apple")))
-                (t (setq os-icon (all-the-icons-fontawesome-4 "desktop"))))
+                 (setq os-icon (nerd-icons-faicon "nf-fa-apple" :height 0.8)))
+                (t (setq os-icon (nerd-icons-faicon "nf-fa-desktop" :height 0.8))))
           (fancy-splash-insert :face '(variable-pitch (:height 0.8) font-lock-builtin-face)
-                               "System Type: "
-                               (kawaii-splash/scale-svg-icon os-icon 0.8)
-                               (upcase (symbol-name system-type))
-                               (if wsl-version
-                                   (concat " on "
-                                           (kawaii-splash/scale-svg-icon (all-the-icons-fontawesome-4 "windows") 0.8)
-                                           "WSL\n")
-                                 "\n"))
+                               "\nSystem Type: ")
+          (insert os-icon)
           (fancy-splash-insert :face '(variable-pitch (:height 0.8) font-lock-builtin-face)
-                               "Window System: " os-icon (upcase (symbol-name window-system)) "\n"))
+                               (upcase (symbol-name system-type)))
+          (when wsl-version
+            (fancy-splash-insert :face '(variable-pitch (:height 0.8) font-lock-builtin-face)
+                                 " on ")
+            (insert (nerd-icons-faicon "nf-fa-windows" :height 0.8))
+            (fancy-splash-insert :face '(variable-pitch (:height 0.8) font-lock-builtin-face)
+                                 "WSL"))
+          (fancy-splash-insert :face '(variable-pitch (:height 0.8) font-lock-builtin-face)
+                               "\n")
+          (fancy-splash-insert :face '(variable-pitch (:height 0.8) font-lock-builtin-face)
+                               "Window System: ")
+          (insert os-icon)
+          (fancy-splash-insert :face '(variable-pitch (:height 0.8) font-lock-builtin-face)
+                               (upcase (symbol-name window-system))
+                               "\n\n"))
         (fancy-splash-insert :face '(variable-pitch (:height 0.9))
-                             emacs-copyright "\n")
-        (skip-chars-backward "\n")
-        (delete-region (point) (point-max))
-        (insert "\n")
+                             emacs-copyright
+                             "\n")
+        ;; End of customized `fancy-startup-tail'
+
         (use-local-map splash-screen-keymap)
         (setq buffer-read-only t)
         (set-buffer-modified-p nil)
         (if (and view-read-only (not view-mode))
             (view-mode-enter nil 'kill-buffer))
-        splash-buffer))))
+        (goto-char (point-min))
+        (forward-line 4)))
+    splash-buffer))
 
 (when (not fancy-splash-image)
   (setq fancy-splash-image
         (concat
          (file-name-directory load-file-name)
          "hacker.png")))
-(setq initial-buffer-choice #'kawaii-splash/splash-screen)
+(setq initial-buffer-choice
+      #'kawaii-splash/splash-screen)
 
 (provide 'kawaii-splash)
 
